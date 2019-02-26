c9aacf7eb41177db2ce3c38008b09330a79504cd
terjr97 <terjr97@aol.com>
2019-02-25 20:19:59 -0500

    I CAN NOT FIGURE OUT HOW TO JUST HAVE THE TEMP WORK WITHOUT THE BUTTON

---
+++ assign3.py
@@ -0,0 +1,33 @@
+#import libraries
+import RPi.GPIO as GPIO
+import Adafruit_DHT
+import time
+import os
+
+#assign pins
+tempPin = 17
+
+t = 1
+
+#temp and humidity sensor
+tempSensor = Adafruit_DHT.DHT11
+
+def readF(tempPin):
+	humidity, temperature = Adafruit_DHT.read_retry(tempSensor, tempPin)
+	temperature = temperature * 9/5.0 +32
+	if humidity is not None and temperature is not None:
+		tempFahr = '{0:0.1f}*F'.format(temperature)
+	else:
+		print('Error reading sensor')
+
+	return tempFahr
+
+try:
+	while t == 1:
+		data = readF(tempPin)
+		print (data)
+
+except KeyboardInterrupt:
+	os.system('clear')
+	print('thank you for using this temp device')
+	GPIO.cleanup()
