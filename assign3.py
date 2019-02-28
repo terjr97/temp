55fb899f5a160b67861beba6df6ad924a6ad5ab2
terjr97 <terjr97@aol.com>
2019-02-27 23:11:55 -0500

    an updated version of the assign3.py that has been improved

--- assign3.py
+++ assign3.py
@@ -1,36 +1,44 @@
-#import libraries
-import RPi.GPIO as GPIO
+
 import Adafruit_DHT
 import time
+import RPi.GPIO as GPIO
 import os
 
-#assign pins
+#Assign gpio pins
 tempPin = 17
-
-t = 1
+redPin = 27
+greenPin = 22
+count = 0
+#initialize the gpio
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(redPin,GPIO.OUT)
+GPIO.setup(greenPin, GPIO.OUT)
 
 #temp and humidity sensor
 tempSensor = Adafruit_DHT.DHT11
 
-def readF(tempPin):
-	humidity, temperature = Adafruit_DHT.read_retry(tempSensor, tempPin)
-	temperature = temperature * 9/5.0 +32
-	if humidity is not None and temperature is not None:
-		tempFahr = '{0:0.1f}*F'.format(temperature)
+while True:
+        humidity, temperature = Adafruit_DHT.read_retry(tempSensor,
+tempPin)
+        temperature = temperature * 9/5.0 +32
+        if humidity is not None and temperature is not None:
+#               tempFahr = '{0:0/1f}*F'.format(temperature)
+                print('Temperature = {0:0.1f}*F'.format(temperature, humidity))
+        else:
+                print('Failed to get reading. Try again!')
+	if temperature > 70  and temperature < 80:
+		GPIO.output(22,True)
+		time.sleep(60)
+		GPIO.output(22,False)
 	else:
-		print('Error reading sensor')
+		count = 60
+		while count > 0:
+			GPIO.output(27,True)
+			time.sleep(.5)
+			GPIO.output(27,False)
+			time.sleep(.5)
+			count = count-1
+	count = 0
+	GPIO.output(22,False)
+	GPIO.output(27,False)
 
-	return tempFahr
-
-try:
-	with open("/log/templog.csv", "a") as log
-		while t == 1:
-			data = readF(tempPin)
-			print (data)
-			time.sleep(60)
-			log.write("str(data)")
-
-except KeyboardInterrupt:
-	os.system('clear')
-	print('thank you for using this temp device')
-	GPIO.cleanup()
