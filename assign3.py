19e8ff7f02bf5bdfb5f23b2a491a6974b786743e
terjr97 <terjr97@aol.com>
2019-02-28 17:22:20 -0500

    The lights should be working now

--- assign3.py
+++ assign3.py
@@ -9,36 +9,41 @@ tempPin = 17
 redPin = 27
 greenPin = 22
 count = 0
+
 #initialize the gpio
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(redPin,GPIO.OUT)
-GPIO.setup(greenPin, GPIO.OUT)
+GPIO.setup(greenPin,GPIO.OUT)
+
+def green(pin):
+	GPIO.output(pin,True)
+	time.sleep(60)
+	GPIO.output(pin,False)
+
+def red(pin):
+	GPIO.output(pin,True)
+	time.sleep(.5)
+	GPIO.output(pin,False)
+	time.sleep(.5)
 
 #temp and humidity sensor
 tempSensor = Adafruit_DHT.DHT11
-
 while True:
-        humidity, temperature = Adafruit_DHT.read_retry(tempSensor,
-tempPin)
+        humidity, temperature = Adafruit_DHT.read_retry(tempSensor,tempPin)
         temperature = temperature * 9/5.0 +32
         if humidity is not None and temperature is not None:
 #               tempFahr = '{0:0/1f}*F'.format(temperature)
                 print('Temperature = {0:0.1f}*F'.format(temperature, humidity))
         else:
                 print('Failed to get reading. Try again!')
-	if temperature > 70  and temperature < 80:
-		GPIO.output(22,True)
-		time.sleep(60)
-		GPIO.output(22,False)
+	if temperature > 68  and temperature < 78:
+		green(22)
 	else:
 		count = 60
 		while count > 0:
-			GPIO.output(27,True)
-			time.sleep(.5)
-			GPIO.output(27,False)
-			time.sleep(.5)
+			red(27)
 			count = count-1
 	count = 0
-	GPIO.output(22,False)
-	GPIO.output(27,False)
+	GPIO.output(redPin,False)
+	GPIO.output(greenPin,False)
 
