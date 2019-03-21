8f3dbc477b6e901d516c810a02eb4bdcf1c7931c
terjr97 <terjr97@aol.com>
2019-03-20 22:23:43 -0400

    More progress on the main program

--- midterm.py
+++ midterm.py
@@ -1,4 +1,4 @@
-import Adafruit_DHT
+#importing the necesarry data
 import time
 import RPi.GPIO as GPIO
 import os
@@ -7,7 +7,7 @@ import sys
 
 con = None
 try:
-        con = mydb.connect('timeTemp')
+        con = mydb.connect('')
         cur = con.cursor()
         cur.execute('SELECT SQLITE_VERSION()')
         data = cur.fetchone()
@@ -17,16 +17,30 @@ except mydb.Error, e:
         print "Error %s:" %e.args[0]
         sys.exit(1)
 
-
-#Assign gpio pins
+#Assign gpio pins. 13 is for entering. 26 is for leaving.
 camPin1 = 13
 camPin2 = 26
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(camPin1, GPIO.IN)
 GPIO.setup(camPin2, GPIO.IN)
-
-
+#let the camera have time to set up
+time.sleep(10)
+try:
+	while True:
+		current = people
+                if GPIO.input(camPin1):
+			people = people + 1
+			time.sleep(2)
+		if GPIO.input(camPin2):
+			people = people - 1
+			time.sleep(2)
+		#decrease in room population
+		if current > people:
+			current = current
+		#increase in room population
+		if current < people:
+			current = current
 
 except KeyboardInterrupt:
         GPIO.cleanup()
